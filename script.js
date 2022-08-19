749bbec98b6461063b5820ffbc9b7ed8c30a9428
tarfah-alshammeri <110110361+tarfah-alshammeri@users.noreply.github.com>
2022-08-19 10:25:21 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+function BMI ( weight , height){
+    let result = weight  / (height * height) 
+    return result 
+}
+console.log(BMI (70,1.70))
+function status (bmi){
+    if(bmi <18.5 ){return ("لديك نقص في الوزن")}
+    else if( bmi >= 18.5 && bmi< 25){
+        return(" وزنك صحي") }
+        else{ 
+            return("لديك زيادة في الوزن")
+        }
+}
+function calculate (params) {let  weight= document.getElementById( "weight").value
+  let height=document.getElementById(height).value
+  let bmi = BMI(weight,height)
+  let disc = Status(bmi)
+  let div = document.getElementById("result")
+  div.innerText = bmi + " == " + desc
+}
